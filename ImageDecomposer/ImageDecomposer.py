860b4f1bce157acacac0df275f29ff4251827e8a
Amarth Gul <amarthgul@gmail.com>
2022-11-15 17:50:36 -0500

    Add files via upload

---
+++ ImageDecomposer/ImageDecomposer.py
@@ -0,0 +1,66 @@
+
+
+import PIL.Image as img
+import os
+
+_DEBUGGING = True
+
+# The following are from another script and has little to do with CSE5544 project.
+# They are put here merely as reference or placeholder 
+
+
+
+
+def getCurrentFolder():
+    current = str(os.path.abspath(__file__))
+    for itera in range(len(current) - 1, 0, -1):
+        if current[itera] == '\\':
+            dir = current[0: itera] #Get current directory
+            break;
+    return dir
+
+def findWebp():
+    '''Find webp fomat files'''
+    files = os.listdir(getCurrentFolder())
+
+    webps = []
+    for f in files:
+        print(f.lower()[-4:])
+        if f.lower()[-4:] == "webp":
+            webps.append(f)
+
+    return webps
+
+
+def openAndConvert(fileNames = [], targetFormat ="png"):
+    '''Given the files, open them and convert to another format and then save'''
+
+    outDir = getCurrentFolder() + '\\OutputImages\\'
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+
+    for name in fileNames:
+        im = img.open(name).convert("RGB")
+        noExtName = name[:-5]
+        im.save(outDir + noExtName + '.' + targetFormat)
+
+#==================================================================
+'''============================================================='''
+
+def Main():  
+    if _DEBUGGING:
+        waitingToConv = findWebp()
+        openAndConvert(waitingToConv)
+        
+    else:
+        try:
+
+            waitingToConv = findWebp()
+            openAndConvert(waitingToConv)
+
+        except (ValueError,NameError,SyntaxError,AttributeError,\
+            TypeError) as err:
+            print('Error!',err);
+
+if __name__ == '__main__':
+    Main()
