d863518687295387b3466858d79fb3a5968b7c75
Amarth Gul <amarthgul@gmail.com>
2022-12-03 18:55:10 -0500

    Added the embeding

--- ImageDecomposer/ColorCube.py
+++ ImageDecomposer/ColorCube.py
@@ -2,51 +2,88 @@ import colorsys
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
-class ColorCube():
-    
-
+class ColorCube(FigureCanvas):
     def __init__(self):
         self.mode = 'rgb'
-        self.plotSize = 6.5
+        self.plotSize = 1
         self.step = 8
         self.pointSize = 240
-        self.pointOpacity = 0.5
+        self.pointOpacity = 0.25
         self.fontSize = 15 
+        self.showLegend = False 
+        self.useBlackBackground = False 
+
+        fig = Figure()
+        self.fig = fig
+        self.axes = fig.add_subplot(111, projection='3d')
 
     def ShowPlot(self):
-        plt.figure(figsize = (self.plotSize, self.plotSize))
-        ax3D = plt.axes([0.05, 0.05, 0.9, 0.9], projection = '3d')
+        self.fig = plt.figure()
+        self.axes = self.fig.add_subplot(111, projection='3d')
 
         # First remove fill
-        ax3D.w_xaxis.set_pane_color((.0, .0, .0, 1.0))
-        ax3D.w_yaxis.set_pane_color((.0, .0, .0, 1.0))
-        ax3D.w_zaxis.set_pane_color((.0, .0, .0, 1.0))
+        self.axes.w_xaxis.set_pane_color((.0, .0, .0, 1.0))
+        self.axes.w_yaxis.set_pane_color((.0, .0, .0, 1.0))
+        self.axes.w_zaxis.set_pane_color((.0, .0, .0, 1.0))
+        self.axes.axis('off')
+        if self.useBlackBackground:
+            self.axes.set_facecolor("black")
 
+        def midpoints(x):
+            sl = ()
+            for i in range(x.ndim):
+                x = (x[sl + np.index_exp[:-1]] + x[sl + np.index_exp[1:]]) / 2.0
+                sl += np.index_exp[:]
+            return x
+
+        start = 0;
+        end = 255;
         if self.mode is 'rgb':
-            start = 0;
-            end = 255;
+            spatialAxes = [self.step, self.step, self.step]
+            r, g, b= np.indices((self.step+1, self.step+1, self.step+1)) / 16.0
+            rc = midpoints(r)
+            gc = midpoints(g)
+            bc = midpoints(b)
+            cube = np.ones(spatialAxes, dtype=np.bool)
+            # combine the color components
+            colors = np.zeros(cube.shape + (4,))
+            colors[..., 0] = rc
+            colors[..., 1] = gc
+            colors[..., 2] = bc
+            colors[..., 3] = 0.2
+            self.axes.voxels(r, g, b, cube,
+                  facecolors = colors, 
+                  linewidth = 0)
 
-            for zValue in np.linspace(start, end, self.step):
-                for xValue in np.linspace(start, end, self.step):
-                    for yValue in np.linspace(start, end, self.step):
-                        color = '#%02x%02x%02x' % (int(xValue), int(yValue), int(zValue));
-                        ax3D.scatter(xValue, yValue, zValue, c = color, s = self.pointSize)
-            ax3D.set_xlabel('$Red$', fontsize = self.fontSize)    
-            ax3D.set_ylabel('$Green$', fontsize = self.fontSize)    
-            ax3D.set_zlabel('$Blue$', fontsize = self.fontSize) 
+            if self.showLegend:
+                self.axes.set_xlabel('$Red$', fontsize = self.fontSize)    
+                self.axes.set_ylabel('$Green$', fontsize = self.fontSize)    
+                self.axes.set_zlabel('$Blue$', fontsize = self.fontSize) 
 
         elif self.mode is 'hsv':
-            for hue in np.linspace(0, 360, step):
-                for sat in np.linspace(0, 1, step):
-                    for val in np.linspace(0, 1, step):
-                        color = cls.hsv_to_rgb([hue / 360.0, sat, val]).reshape(1,-1)
-                        ax3D.scatter(hue, sat, val, c = color, s = self.pointSize)
-            ax3D.set_xlabel('$Hue$', fontsize = self.fontSize)    
-            ax3D.set_ylabel('$Saturation$', fontsize = self.fontSize)    
-            ax3D.set_zlabel('$Value$', fontsize = self.fontSize)
+            for hue in np.linspace(0, 360, self.step):
+                for sat in np.linspace(0, 1, self.step):
+                    for val in np.linspace(0, 1, self.step):
+                        color = colorsys.hsv_to_rgb(hue / 360.0, sat, val)
+                        color = '#%02x%02x%02x' % (int(color[0] * end), 
+                                                   int(color[1]* end), 
+                                                   int(color[2]* end));
+                        self.axes.scatter(hue, sat, val, 
+                                          c = color, 
+                                          s = self.pointSize, 
+                                          alpha = self.pointOpacity)
+            if self.showLegend:
+                self.axes.set_xlabel('$Hue$', fontsize = self.fontSize)    
+                self.axes.set_ylabel('$Saturation$', fontsize = self.fontSize)    
+                self.axes.set_zlabel('$Value$', fontsize = self.fontSize)
 
-        plt.show()
+        if __name__ == "__main__":
+            plt.show()
+
+        return self.axes
 
 
 
@@ -57,3 +94,18 @@ def main():
 
 if __name__ == "__main__":
     main() 
+
+
+
+'''
+    Unused code 
+
+            for zValue in np.linspace(start, end, self.step):
+                for xValue in np.linspace(start, end, self.step):
+                    for yValue in np.linspace(start, end, self.step):
+                        color = '#%02x%02x%02x' % (int(xValue), int(yValue), int(zValue));
+                        self.axes.scatter(xValue, yValue, zValue, 
+                                          c = color, 
+                                          s = self.pointSize, 
+                                          alpha = self.pointOpacity)
+'''
---
+++ ImageDecomposer/PlotEmbedding.py
@@ -0,0 +1,82 @@
+
+# This file is for testing plot embedding in PyQt5
+import os
+import sys
+
+
+import pandas as pd
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtWidgets import (QFileDialog, 
+                             QDialog, QApplication, QWidget, QMainWindow, 
+                             QAction, QVBoxLayout, QSizePolicy)
+
+
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+from UI_MainWindow import Ui_MainWindow
+from ColorCube import ColorCube 
+
+
+
+_DEBUGGING = False
+
+#class MplCanvas(FigureCanvas):
+
+#    def __init__(self, parent=None, width=5, height=4, dpi=100):
+#        fig = Figure(figsize=(width, height), dpi=dpi)
+#        self.axes = fig.add_subplot(111)
+#        super(MplCanvas, self).__init__(fig)
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+        cc_rgb = ColorCube()
+        layout_rgb = self.RGB_Area.layout()
+        if layout_rgb is None:
+            layout_rgb = QVBoxLayout(self.RGB_Area)
+        ax_rgb = cc_rgb.ShowPlot()
+        canvas_rbg = FigureCanvas(ax_rgb.figure)
+        ax_rgb.mouse_init()
+        layout_rgb.addWidget(canvas_rbg)
+
+        cc_hsv = ColorCube()
+        cc_hsv.mode = 'hsv'
+        layout_hsv = self.HSV_Area.layout()
+        if layout_hsv is None:
+            layout_hsv = QVBoxLayout(self.HSV_Area)
+        ax_hsv = cc_hsv.ShowPlot()
+        canvas_hsv = FigureCanvas(ax_hsv.figure)
+        ax_hsv.mouse_init()
+        layout_hsv.addWidget(canvas_hsv)
+
+
+
+def Main():  
+    if _DEBUGGING:
+        print("CSE 5544")
+        
+    else:
+        app = QtWidgets.QApplication(sys.argv)
+
+        window = MainWindow()
+        window.show()
+        app.exec()
+
+
+if __name__ == '__main__':
+    Main()
